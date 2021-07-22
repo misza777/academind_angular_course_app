026a5f935578f9b38d6c1b49eca2c3dc2541995c
misha novakishvili <46324818+misza777@users.noreply.github.com>
2021-07-22 12:35:43 +0200

    navbar components recipeModel

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  // budujemy model recepty
+recipes: Recipe[] = [
+  new Recipe('Zupa Pomidorowa', 'Superowa zupa dla kazdego!', "https://www.mniammniam.com/obrazki/zupa_pomidorowa.jpg" )
+];
   constructor() { }
 
   ngOnInit(): void {
