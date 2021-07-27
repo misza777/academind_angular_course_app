8d7b79219a488570d31c9f6ba27a00f9903c657e
misha novakishvili <46324818+misza777@users.noreply.github.com>
2021-07-27 14:03:58 +0200

    viewchild binding in shopping-list-edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter  } from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+// odczytanie wartoscie referencji
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+//emit event
+@Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onAddItem() {
+    // const bo nie zmieniamy wartosci zmiennych
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    // emisja eventu
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
